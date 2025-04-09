c05ef24cb52492d29c67a2d85b889a53de3f51ee
Caio Rafael <167817347+caioetz@users.noreply.github.com>
2025-04-09 15:00:44 -0300

    Add files via upload

--- php.php
+++ php.php
@@ -1,8 +1,25 @@
-<div id="a">
-    <h1>Dados Coletados</h1>
-     <?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>php</title>
+    <style>
+    {
+        align-items: center;
+    }
+</style>
+</head>
+<body>
 
-     
+    
+
+    
+
+    
+<div style="align-items: center; margin-left: 40%; margin-top: 2%;  margin-right: 40%; padding: auto;  border-radius: 10px; background-color: rgb(218, 218, 218);">
+<div style="margin-left: 7%;">
+<h1>Dados Coletados</h1>
+     <?php
     echo "Nome: ";
     echo $_GET['nome'];
     echo "<br>"; 
@@ -31,7 +48,7 @@
     echo $_GET['nivel_html'];
     echo "<br>";
     
-    echo "SO: ";
+    echo "Sistema Operacional: ";
     echo $_GET['sistema'];
     echo "<br>";
     ?>
@@ -39,4 +56,6 @@
  <br>
  <a href="index.html">Voltar</a>
 </div>
- 
+ </div>
+</body>
+</html>
